0ae2935805031706220f95c8f7a61134c2dfa0b0
ninatai <ninatai@cairo.csl.toronto.edu>
2017-05-31 15:24:20 -0700

    change to default response size

--- webserver.py
+++ webserver.py
@@ -6,7 +6,7 @@ class CustomResponseHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
         self.end_headers()
-        self.wfile.write("a" * 40000)
+        self.wfile.write("a" * 9000)
         self.wfile.close()
 
 httpd = SocketServer.TCPServer(("", 80), CustomResponseHandler)
